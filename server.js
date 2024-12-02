c919809f442061191c6e09474e0916058a432251
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-12-02 20:52:07 +0300

    render on the server and import client-side js

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 import { createServer } from "node:http";
+
 import appComponent from "./ssr.js";
 
-const server = createServer((_req, res) => {
-  res.write(appComponent());
+const server = createServer((req, res) => {
+  if (req.url === "/app.js") {
+    const app = fs.readFileSync(
+      path.join(fileURLToPath(import.meta.url), "../app.js"),
+      "utf-8"
+    );
+    res.setHeader("Content-Type", "text/javascript");
+    res.write(app);
+    res.end();
+    return;
+  }
+
+  res.write(`
+    <html>
+        <body>
+            <div id="app">
+                ${appComponent()}
+            </div>
+            <script type="module">
+                import App from "./app.js";
+                App().create(document.getElementById("app"));
+            </script>
+        </body>
+    </html>
+    `);
   res.end();
 });
 
